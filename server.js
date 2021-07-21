c231fd37f9d2b253ebc4e620bc74c300b8c7f95c
Alee Herasimiuk <aleeherasimiuk@gmail.com>
2021-07-20 23:58:25 -0300

    Now client needs to provide secret key to turn on/off the lights

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ const express = require('express');
 const config  = require('./config');
 const app = express();
 
+app.use( express.json() );
+app.use(express.urlencoded({
+  extended: true
+})); 
+
 const connection = new ewelink({
   email: config.email,
   password: config.password,
   region: config.region,
 });
 
-app.get('/ewelink/toggle-lights-state', (req, res) => {
+app.post('/ewelink/toggle-lights-state', (req, res) => {
+  const key = req.body.secret_key;
+  if (key !== config.secret_key) {
+    res.status(401).send('Unauthorized');
+    return;
+  }
   (async () => await connection.toggleDevice(config.device_id))();
-  res.sendStatus(200);
+  res.status(200).send('Ok');
 });
 
 app.listen(24444, function () {
